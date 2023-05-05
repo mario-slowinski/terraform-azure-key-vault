c07cf376872a8d14daa5af0cec179f0e04eccc4b
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-05-05 20:54:22 +0200

    Move "certificate_contacts" to separate file.

---
+++ certificate_contacts.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault_certificate_contacts" "many" {
+  key_vault_id = azurerm_key_vault.this.id
+
+  dynamic "contact" {
+    for_each = var.certificate_contacts
+    content {
+      email = contact.value.email
+      name  = contact.value.name
+      phone = contact.value.phone
+    }
+  }
+
+  depends_on = [
+    azurerm_key_vault_access_policy.many,
+  ]
+}

076d70622a8434d3dd6e2b7c1f080104746b4977
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-02-08 22:16:25 +0100

    Add "key_vault_id" to "secrets" vars.

--- secret.vars.tf
+++ secret.vars.tf
@@ -2,6 +2,7 @@ variable "secrets" {
   type = list(object({
     name            = string                # Specifies the name of the Key Vault Secret. Changing this forces a new resource to be created.
     value           = optional(string)      # Specifies the value of the Key Vault Secret or random generated. Changing this will create a new version of the Key Vault Secret.
+    key_vault_id    = optional(string)      # The ID of the Key Vault where the Secret should be created.
     content_type    = optional(string)      # Specifies the content type for the Key Vault Secret.
     tags            = optional(map(string)) # A mapping of tags to assign to the resource.
     not_before_date = optional(string)      # Key not usable before the provided UTC datetime (Y-m-d'T'H:M:S'Z').

1b5915cb686032958348c97fb3ad4021f2b1076b
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-02-08 22:17:07 +0100

    Output "secrets" data, without secret value.

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "data" {
   value       = try(azurerm_key_vault.name[var.name], null)
   sensitive   = false
 }
+
+output "secrets" {
+  description = "The list of key vault secrets."
+  value       = { for name, secret in azurerm_key_vault_secret.name : name => merge(secret, { value = null }) }
+  sensitive   = false
+}

4178d911d8d422551d78c8fe51af2fb3763943ba
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-02-28 16:29:45 +0100

    Switch "certificate_contacts" from multiple resources to multiple blocks in single resource.

--- certificate_contacts.main.tf
+++ certificate_contacts.main.tf
@@ -1,19 +1,20 @@
-resource "azurerm_key_vault_certificate_contacts" "email" {
-  for_each = {
-    for certificate_contact in var.certificate_contacts :
-    certificate_contact.email => certificate_contact
-    if certificate_contact.email != null
-  }
-
-  contact {
-    email = each.key
-    name  = each.value.name != "" ? each.value.name : null
-    phone = each.value.phone != "" ? each.value.phone : null
-  }
+resource "azurerm_key_vault_certificate_contacts" "list" {
   key_vault_id = azurerm_key_vault.name[var.name].id
+
+  dynamic "contact" {
+    for_each = {
+      for certificate_contact in var.certificate_contacts :
+      certificate_contact.email => certificate_contact
+      if certificate_contact.email != null
+    }
+    content {
+      email = contact.value.email
+      name  = contact.value.name
+      phone = contact.value.phone
+    }
+  }
 
   depends_on = [
     azurerm_key_vault.name,
-    azurerm_key_vault_access_policy.object_id,
   ]
 }
